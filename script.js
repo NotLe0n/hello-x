2a2496c3e1998733de6ea751ed5947ed56aaa2ec
NotLe0n <notle0n75@gmail.com>
2022-10-02 13:01:59 +0200

    cleanup

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 updateBackground(); // set background
 
 updateTime(); // set time at start
-setInterval(updateTime, 1000) // update the current time every second
+setInterval(updateTime, 1000); // update the current time every second
 
-updateBattery() // set battery icon
+updateBattery(); // set battery icon
 if (lightdm.battery_data !== undefined && lightdm.battery_update !== undefined) {
-    lightdm.battery_update.connect(updateBattery) // update battery info every status change
+    lightdm.battery_update.connect(updateBattery); // update battery info every status change
 }
 
 populateUserSelection();
 
 // selects a random background each time
 function updateBackground() {
-    const images = ['clouds', 'konstanz', 'sunset'] // image names array
+    const images = ['clouds', 'konstanz', 'sunset']; // image names array
     const index = Math.floor(Math.random() * 3); // select random number from 0-3
 
-    console.log('current background: ' + images[index])
+    console.log('current background: ' + images[index]);
     // set --bg variable to random element of the image names array
     document.documentElement.style.setProperty(
         '--bg',
@@ -26,7 +26,12 @@ function updateBackground() {
 // updates the current time
 function updateTime() {
     const today = new Date();
-    document.getElementById("current-time").innerHTML = today.toLocaleTimeString([], { hour12: false, hour: '2-digit', minute: '2-digit', second: "2-digit" });
+    document.getElementById("current-time").innerHTML = today.toLocaleTimeString([], {
+        hour12: false,
+        hour: '2-digit',
+        minute: '2-digit',
+        second: "2-digit"
+    });
 }
 
 function updateBattery() {
@@ -34,60 +39,61 @@ function updateBattery() {
         return;
     }
 
-    console.log('updating battery info...')
+    console.log('updating battery info...');
 
     // round down to the nearest multiple of 10
-    const batteryLevel = lightdm.battery_data.level - lightdm.battery_data.level % 10
+    const batteryLevel = lightdm.battery_data.level - lightdm.battery_data.level % 10;
     const img = document.getElementById('battery-status-icon');
 
     // set icon
     img.setAttribute(
         'src',
         `img/icons/battery-${batteryLevel}.svg`
-    )
+    );
 
     // set title
     img.setAttribute(
         'title',
         `${lightdm.battery_data.level}%`
-    )
+    );
+
     const text = document.getElementById('battery-status-text');
-    text.innerText = `${lightdm.battery_data.level}%`
+    text.innerText = `${lightdm.battery_data.level}%`;
 }
 
 function populateUserSelection() {
-    const users = lightdm.users
-    const userSelect = document.getElementById('user')
+    const users = lightdm.users;
+    const userSelect = document.getElementById('user');
 
     for (const user of users) {
         let opt = document.createElement('option');
-        opt.innerHTML = user.username
+        opt.innerHTML = user.username;
 
-        userSelect.appendChild(opt)
+        userSelect.appendChild(opt);
     }
 }
 
 function submitPass() {
     let elm = document.getElementById("pass");
-    console.log("password submitted")
+    console.log("password submitted");
 
     setTimeout(async () => {
-        const user = document.getElementById('user').innerText
-        
-        let authSent = lightdm.authenticate(user)
-        console.log(`authenticated ${user}: ${authSent}`)
+        const user = document.getElementById('user').innerText;
 
-        await wait(100) // mandatory
+        let authSent = lightdm.authenticate(user);
+        console.log(`authenticated ${user}: ${authSent}`);
+
+        await wait(100); // mandatory
 
         let responseSent = lightdm.respond(elm.value);
         console.log(`responded with ${elm.value}: ${responseSent}`)
-        console.log('starting session...')
+        console.log('starting session...');
 
-        await wait(100) // mandatory
-        lightdm.start_session()
+        await wait(100); // mandatory
+        lightdm.start_session();
     }, 1000);
 }
 
 function wait(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms))
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
