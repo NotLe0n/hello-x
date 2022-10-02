89895ad39ca3e1da3fd4842d1bc1c4d23720ff8f
NotLe0n <notle0n75@gmail.com>
2022-10-02 12:55:08 +0200

    battery info now displays percentage as text

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ updateBackground(); // set background
 updateTime(); // set time at start
 setInterval(updateTime, 1000) // update the current time every second
 
-updateBatteryIcon() // set battery icon
-setInterval(updateBatteryIcon, 60000) // update the battery icon every minute
+updateBattery() // set battery icon
+if (lightdm.battery_data !== undefined && lightdm.battery_update !== undefined) {
+    lightdm.battery_update.connect(updateBattery) // update battery info every status change
+}
 
 populateUserSelection();
 
@@ -13,6 +15,7 @@ function updateBackground() {
     const images = ['clouds', 'konstanz', 'sunset'] // image names array
     const index = Math.floor(Math.random() * 3); // select random number from 0-3
 
+    console.log('current background: ' + images[index])
     // set --bg variable to random element of the image names array
     document.documentElement.style.setProperty(
         '--bg',
@@ -26,14 +29,16 @@ function updateTime() {
     document.getElementById("current-time").innerHTML = today.toLocaleTimeString([], { hour12: false, hour: '2-digit', minute: '2-digit', second: "2-digit" });
 }
 
-function updateBatteryIcon() {
+function updateBattery() {
     if (lightdm.battery_data === undefined) {
         return;
     }
 
+    console.log('updating battery info...')
+
     // round down to the nearest multiple of 10
     const batteryLevel = lightdm.battery_data.level - lightdm.battery_data.level % 10
-    const img = document.getElementById('battery-status');
+    const img = document.getElementById('battery-status-icon');
 
     // set icon
     img.setAttribute(
@@ -46,11 +51,14 @@ function updateBatteryIcon() {
         'title',
         `${lightdm.battery_data.level}%`
     )
+    const text = document.getElementById('battery-status-text');
+    text.innerText = `${lightdm.battery_data.level}%`
 }
 
 function populateUserSelection() {
     const users = lightdm.users
     const userSelect = document.getElementById('user')
+
     for (const user of users) {
         let opt = document.createElement('option');
         opt.innerHTML = user.username
