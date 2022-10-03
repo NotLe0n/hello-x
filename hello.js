9c183ffe5ce8823473902e1339623bed732f656a
NotLe0n <notle0n75@gmail.com>
2022-10-03 20:30:33 +0200

    fixed hello not working with unicode

--- hello.js
+++ hello.js
@@ -3,14 +3,14 @@ hello = {
 	"de": "Hallo",
 	"nl": "Hallo",
 	"no": "Hallo",
-	"ru": "Привет",
+	"ru": "\u041f\u0440\u0438\u0432\u0435\u0442",
 	"pl": "Witam",
 	"fr": "Bonjour",
 	"it": "Bonjour",
-	"sv": "Hallå",
+	"sv": "Hall\u00e5",
 	"es": "Hola",
 	"tr": "Merhaba",
-	"ko": "안녕하십니까",
-	"jp": "こんにちは",
-	"ch": "你好"
+	"ko": "\uc548\ub155\ud558\uc2ed\ub2c8\uae4c",
+	"jp": "\u3053\u3093\u306b\u3061\u306f",
+	"ch": "\u4f60\u597d"
 }
